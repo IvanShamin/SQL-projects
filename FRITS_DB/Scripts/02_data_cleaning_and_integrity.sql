4016a33ba22163c63cb0d05094c5e0c689a2dc84
IvanShamin <vansoshamin@gmail.com>
2026-01-21 16:26:11 +0100

    Delete incorrect FRIT variation and related records
    Remove incorrect FRIT variation 'VO6273' and its links.

--- FRITS_DB/Scripts/02_data_cleaning_and_integrity.sql
+++ FRITS_DB/Scripts/02_data_cleaning_and_integrity.sql
@@ -19,3 +19,9 @@ INSERT INTO slozeni (id_pol, id_sur, mnozstvi) VALUES
 ('2042', '1', '54.18'), ('2042', '4', '14.16'), ('2042', '5', '7.83'), 
 ('2042', '13', '12.69'), ('2042', '6', '5.01'), ('2042', '7', '0.65'), 
 ('2042', '16', '2.81'), ('2042', '17', '1.34'), ('2042', '18', '1.33');
+
+-- 2.4  REMOVING INCORRECT FRIT VARIATION: 'VO6273' (ID: 2380)
+Remove the chemical composition links first
+DELETE FROM slozeni WHERE id_pol = 2380;
+ Remove the master record from the frits table
+DELETE FROM frity WHERE id = 2380;
